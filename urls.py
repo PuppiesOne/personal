47ea80eb4077d73b0a6cee3e11fc71a8d9acdb7d
John Miller <John.Miller@mecglobal.com>
2017-03-29 11:29:50 -0400

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from Codify import views
-
-urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
-                       url(r'^Codify/jobcall$', views.jobcall, name='jobcall'),
-                       url(r'^Codify/CAupload$', views.upload_file, name='CAupload'),
-                       )
